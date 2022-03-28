6fc730002d6c19ab1762aaa7eb41b1fc637ec584
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-03-28 17:00:13 +0600

    Tailwind added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1 className='text-6xl my-8 text-blue-600 font-mono font-bold'>Hello React Tailwind</h1>
     </div>
   );
 }
